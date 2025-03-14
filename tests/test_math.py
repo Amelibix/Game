a97aef06e87bce63eaeca2b6b735ab48224587ca
Amélie Brun <am.brun@lyon.ort.asso.fr>
2025-03-14 09:45:05 +0100

    Ajout test

---
+++ tests/test_math.py
@@ -0,0 +1,13 @@
+from maths import math
+
+
+def test_add():
+    assert math.add(7, 3) == 10
+    assert math.add(7) == 9
+    assert math.add(5) == 7
+
+
+def test_product():
+    assert math.product(7, 3) == 21
+    assert math.product(7) == 14
+    assert math.product(5) == 10
